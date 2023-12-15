62463fe07fbb264f851260b0575ba86e629b46dc
Roghan95 <p90svborz@gmail.com>
2023-12-15 09:30:30 +0100

    fixes

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,95 +17,95 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
-public function buildForm(FormBuilderInterface $builder, array $options): void
-{
-$builder
-    ->add(
-        'email',
-        EmailType::class,
-        [
-            'label' => 'Ton adresse email *',
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'exemple@exemple.com',
-                'aria-label' => 'Adresse email'
-            ],
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Entrez une adresse email',
-                ]),
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'Ton adresse email *',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'exemple@exemple.com',
+                        'aria-label' => 'Adresse email'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Entrez une adresse email',
+                        ]),
 
-                new Regex([
-                    'pattern' => '/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/',
-                    'message' => 'Le format de l\'adresse email est invalide.'
-                ]),
-            ],
-        ]
-    )
-    ->add(
-        'pseudo',
-        TextType::class,
-        [
-            'label' => 'Ton pseudo *',
-            'required' => true,
-            'attr' => [
-                'class' => 'form-pseudo',
-                'placeholder' => 'Pseudo',
-                'aria-label' => 'Pseudo'
-            ]
-        ]
-    )
-    ->add('plainPassword', RepeatedType::class, [
-        'mapped' => false,
-        'type' => PasswordType::class,
-        'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
-        'required' => true,
-        'first_options'  => [
-            'label' => 'Ton mot de passe *',
-            'attr' => [
-                'class' => 'password-field',
-                'placeholder' => '••••••••',
-                'aria-label' => 'Mot de passe'
-            ]
-        ],
-        'second_options' => [
-            'label' => 'Répète ton mot de passe *',
-            'attr' => [
-                'class' => 'password-field',
-                'placeholder' => '••••••••',
-                'aria-label' => 'Confirmation du mot de passe'
-            ]
-        ],
-        'constraints' => [
-            new NotBlank([
-                'message' => 'Entrez un mot de passe',
-            ]),
-            new Regex([
-                'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/',
-                'message' => 'Le mot de passe doit contenir au moins 12 caractères, incluant une majuscule, une minuscule, un chiffre et un caractère spécial.'
+                        new Regex([
+                            'pattern' => '/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/',
+                            'message' => 'Le format de l\'adresse email est invalide.'
+                        ]),
+                    ],
+                ]
+            )
+            ->add(
+                'pseudo',
+                TextType::class,
+                [
+                    'label' => 'Ton pseudo *',
+                    'required' => true,
+                    'attr' => [
+                        'class' => 'form-pseudo',
+                        'placeholder' => 'Pseudo',
+                        'aria-label' => 'Pseudo'
+                    ]
+                ]
+            )
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'Ton mot de passe *',
+                    'attr' => [
+                        'class' => 'password-field',
+                        'placeholder' => '••••••••',
+                        'aria-label' => 'Mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Répète ton mot de passe *',
+                    'attr' => [
+                        'class' => 'password-field',
+                        'placeholder' => '••••••••',
+                        'aria-label' => 'Confirmation du mot de passe'
+                    ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un mot de passe',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/',
+                        'message' => 'Le mot de passe doit contenir au moins 12 caractères, incluant une majuscule, une minuscule, un chiffre et un caractère spécial.'
+                    ])
+                ],
             ])
-        ],
-    ])
-    ->add('agreeTerms', CheckboxType::class, [
-        'mapped' => false,
-        'attr' => [
-            'class' => 'form-check-input',
-            'aria-label' => 'Accepter les conditions d\'utilisation'
-        ],
-        'required' => true,
-        'constraints' => [
-            new IsTrue([
-                'message' => 'Vous devez accepter les conditions générales d\'utilisation.',
-            ]),
-        ],
-        'label' => 'J\'accepte les conditions d\'utilisation *',
-    ]);
-}
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                    'aria-label' => 'Accepter les conditions d\'utilisation'
+                ],
+                'required' => true,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions générales d\'utilisation.',
+                    ]),
+                ],
+                'label' => 'J\'accepte les conditions d\'utilisation *',
+            ]);
+    }
 
-public function configureOptions(OptionsResolver $resolver): void
-{
-$resolver->setDefaults([
-    'data_class' => Utilisateur::class,
-]);
-}
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
 }
