9d0c7e06e6609ac297d03ed65ea5ae81f1ab96d0
Roghan95 <p90svborz@gmail.com>
2024-01-24 10:56:04 +0100

    commentaire sur la fonction newMessage

--- src/Form/TournoiType.php
+++ src/Form/TournoiType.php
@@ -44,7 +44,7 @@ class TournoiType extends AbstractType
                 'attr'   => [
                     'min' => (new \DateTime())->format('Y-m-d H:i'),
                     'class' => 'dateDebut'
-                    ]
+                ]
             ])
             ->add('dateFin', DateTimeType::class, [
                 'input' => 'datetime_immutable',
@@ -54,7 +54,7 @@ class TournoiType extends AbstractType
                 'attr'   => [
                     'min' => (new \DateTime())->format('Y-m-d H:i'),
                     'class' => 'dateFin'
-                    ]
+                ]
 
             ])
             // NbJoueursMax
@@ -62,8 +62,8 @@ class TournoiType extends AbstractType
                 'label' => 'Nombre de joueurs maximum * ',
                 'required' => true,
                 'attr' => [
-                    'min' => 1, 
-                    'max' => 1000, 
+                    'min' => 1,
+                    'max' => 1000,
                     'step' => 1,
                     'class' => 'nombreJoueursMax',
                     'placeholder' => 'Autorisé entre 1 et 1000 joueurs.'
@@ -78,18 +78,47 @@ class TournoiType extends AbstractType
                 ],
             ])
             ->add('logoFile', VichImageType::class, [
-                'label' => 'Logo du tournoi *: ',
+                'label' => 'Logo du tournoi (5Mo max) *: ',
                 'required' => true,
                 'attr' => [
-                    'class' => 'logoTournoi'
+                    'class' => 'logoTournoi',
+                    'placeholder' => 'jpg, png, jpeg, gif, webp',
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/gif',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un fichier de type png, jpeg, jpg, gif ou webp.',
+                        'maxSizeMessage' => 'La taille du fichier ne doit pas dépasser 5Mo.'
+                    ])
                 ]
-
             ])
             ->add('banniereTrFile', VichImageType::class, [
-                'label' => 'Bannière *: ',
+                'label' => 'Bannière (5Mo max) *: ',
                 'required' => true,
                 'attr' => [
-                    'class' => 'banniereTournoi'
+                    'class' => 'banniereTournoi',
+                    'placeholder' => 'jpg, png, jpeg, gif, webp'
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/gif',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un fichier de type png, jpeg, jpg, gif ou webp.',
+                        'maxSizeMessage' => 'La taille du fichier ne doit pas dépasser 5Mo.'
+                    ])
                 ]
             ])
             ->add('region', TextType::class, [
